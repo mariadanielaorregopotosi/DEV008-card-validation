9a5703ff612f3933031e9b92573a306bf1b8a4d3
maria daniela <orregodaniela8@gmail.com>
2023-05-20 17:55:42 -0500

    proyecto terminado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-import validator from './validator.js';
+import validator from "./validator.js";
+// creo una cont la cual llamo button
+//document permite interactuar con el DOM (Modelo de Objetos del Documento) para realizar
+//tareas como la selección de elementos, nuestro elemento seria ("Button");
+const button = document.getElementById("Button");
 
-console.log(validator);
+button.addEventListener("click", miFuncion);
+
+function miFuncion(event) {
+  event.preventDefault();
+
+  // Obtener el valor del número de tarjeta ingresado
+  const numeroTarjeta = document.getElementById("tarjeta").value;
+  // Llamar a la función de validación de tarjeta
+  const isValid = validator.isValid(numeroTarjeta);
+
+  // Dentro de la función de validación de la tarjeta
+  //mensaje de alerta si es valido o no
+  if (isValid) {
+    alert("La tarjeta es válida");
+  } else {
+    alert("La tarjeta es inválida");
+  }
+}
+
+// se crea una variable llamada maskify se asigna el valor de la cadena de texto "########".
+let nuevoValor = "########";
+const maskify = prompt("Ingresa el nuevo valor para maskify:");
+// if se agrega una declaracion llamada nuevo valor y luego le indicamos 
+//que el nuevo valor es igual al maskify
+if (nuevoValor) {
+  nuevoValor = maskify;
+}
+// Asignar el nuevo valor a maskify
+function doSomething(maskify) {
+  nuevoValor = maskify
+}
+
+// Llamar a la función La función puede realizar cualquier tarea o
+// cálculo específico que necesites en tu programa.
+doSomething(nuevoValor);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,71 @@
+//se crea una const el cual no se puede moditicar es un valor unico
 const validator = {
-  // ...
+  // se crea una de las pruebas unitarias isValid(una funcion) acepta un parametro llamado numeroDeTarjeta
+  //representa el numero de tarjeta a validar
+  isValid: function (numeroTarjeta) {
+    // 0:CONVIERTO EN ARRAY se crea una contante se le da el nombre de convertir numero en arreglo
+    //esto es igua al parametro o numero el cual le indicamos que nos lo convierta a un array
+    const convertirNumeroEnArreglo = numeroTarjeta.split("");
+    //1:REVERSO NUMERO TARJETA
+    //creamos otra const le damos el nombre de tarjetaRverses y indicamos que queremos reversar
+    //el convertirNumeroEnArreglo y aplicamos el . reverse
+    const tarjetaReverse = convertirNumeroEnArreglo.reverse();
+    // 2: Doble cada segundo dígito
+    const tarjetaDoblarNumero = [];
+    //El método forEach() ejecuta la función indicada una vez por cada elemento del array.
+    tarjetaReverse.forEach(function (elemento, index) {
+      if (index % 2 !== 0) {
+        const doble = elemento * 2;
+        return tarjetaDoblarNumero.push(doble.toString());
+      }
+      return tarjetaDoblarNumero.push(elemento);
+    });
+    //3:TARJETA LISTA PARA SUMAR
+    const tarjetaListaParaSumar = [];
+    tarjetaDoblarNumero.forEach(function (elemento) {
+      if (elemento > 9) {
+        let sumaDeDigitos = 0;
+        const elementoConvertidoArreglo = elemento.split("");
+        elementoConvertidoArreglo.forEach(function (digito) {
+          sumaDeDigitos = sumaDeDigitos + Number(digito);
+        });
+        return tarjetaListaParaSumar.push(sumaDeDigitos);
+      }
+      return tarjetaListaParaSumar.push(Number(elemento));
+    });
+
+    // 3: Suma los dígitos
+    let totalSuma = 0;
+    tarjetaListaParaSumar.forEach(function (elemento) {
+      totalSuma = totalSuma + elemento;
+    });
+
+    //const suma = tarjetaArray.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+    if (totalSuma % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+    // 4: Verifica si es válido
+    // const esValida = suma % 10 === 0; //condicional para retornar en verdadero;
+  },
+
+  maskify(numeroTarjeta) {
+    //se utiliza para almacenar el valor de la longitud de una cadena de texto. elemplo un numero de tarjeta
+    //numeroTarjeta es una cadena de texto que representa un número de tarjeta
+    //numeroTarjeta.length, obtendremos el número de caracteres en esa cadena.
+    //La propiedad length es una propiedad incorporada la cantidad de elementos en un array
+    const longitud = numeroTarjeta.length;
+    const ultimosCuatroDigitos = numeroTarjeta.substr(-4); // Obtener los últimos cuatro dígitos
+
+    let caracteresEnmascarados = "";
+    for (let i = 0; i < longitud - 4; i++) {
+      caracteresEnmascarados += "#"; 
+      // Agregar un carácter de enmascaramiento por cada dígito excepto los últimos cuatro
+    }
+
+    return caracteresEnmascarados + ultimosCuatroDigitos;
+  },
 };
 
 export default validator;
